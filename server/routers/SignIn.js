85736836a6efe2b83ca2414536c402c88f20dcdf
michalk1924 <michalk1924@gmail.com>
2024-06-03 21:22:51 +0300

    Account Acess in server

--- server/routers/SignIn.js
+++
@@ -1,8 +0,0 @@
-const electricalProductsController = require('../controllers/electricalProductsController')
-
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => await SignIn.getAll(req ,res, ()=>{}));
-
-module.exports = router;
