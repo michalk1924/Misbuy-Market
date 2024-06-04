089fd499279924378fc92ebc2e9d39315a5bc0a4
Yoni4517 <yonistern15@gmail.com>
2024-06-05 00:07:03 +0300

    singlr item

--- client/src/components/ItemDetails.jsx
+++ client/src/components/ItemDetails.jsx
@@ -5,19 +5,22 @@ import { useParams } from 'react-router-dom';
 
 
 
-const ItemDetails = ({ customer }) => {
-  async function getItem(id) {
-    const url = `http://localhost:3000/api/electricalProducts/${id}`
-    const response = await fetch(url)
-    const data = await response.json()
-    setItem(data)
-  }
+function ItemDetails() {
   const [item, setItem] = useState({});
-  
+  let {itemId} = useParams(); // Accessing URL parameter directly
+
+  async function getItem(id) {
+    if (id) {
+      const url = `http://localhost:3000/api/electricalProducts/${id}`
+      const response = await fetch(url)
+      const data = await response.json()
+      setItem(data)
+    }
+  }
+
   useEffect(() => {
-    const {id}=useParams();
-    getItem(id)
-  }, []);
+    getItem(itemId)
+  }, [itemId]);
 
   return (
     <div className="details-container">
@@ -26,9 +29,11 @@ const ItemDetails = ({ customer }) => {
       </div>
       <div className="info-container">
         <div>
+          <p><strong>id:</strong> {item && item._id}</p>
           <p><strong>Category:</strong> {item && item.category}</p>
           <p><strong>Description:</strong> {item && item.description}</p>
           <p><strong>Price:</strong> {item && item.price}</p>
+          <p><strong>Area:</strong> {item && item.area}</p>
           {/* <p><strong>Name:</strong> {customer.name}</p>
           <p><strong>Email:</strong> {customer.email}</p>
           <p><strong>Phone:</strong> {customer.phone}</p>
