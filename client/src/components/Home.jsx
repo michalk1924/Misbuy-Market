f4849e6880544ac1df1790eb0f773288e6b93d24
michalk1924 <michalk1924@gmail.com>
2024-07-01 17:09:48 +0300

    category style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,56 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CategoryBox from './CategoryBox';
 import '../style/home.css'
+import { TailSpin } from 'react-loader-spinner';
+import ItemBox from './ItemBox';
+import accessoriesImage from '../images/Accessories.png';
+import shoesImage from '../images/Shoes.png';
+import clothesImage from '../images/Clothes.png';
 
 function Home() {
+
+  const [pouplerItems, setPouplerItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getMostPouplerItems() {
+    setLoading(true);
+    const url = `http://localhost:3000/api/allItems`;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      const mostPouplerItems = data.sort(i => i.viewsCounter)
+      if (data.length > 0) {
+        setPouplerItems(mostPouplerItems)
+        setLoading(false);
+      }
+    } else {
+      setPouplerItems([])
+      alert("error fetching!")
+    }
+  }
+
+  useEffect(() => {
+    getMostPouplerItems();
+  }, [])
 
   return (
     <div>
       <h1 id="title">Misbuy Market</h1>
       <div id="categories">
-        <CategoryBox category="Shoes" backgroundColor="black" linkTo="../items/shoes"/>
-        <CategoryBox category="Clothes" backgroundColor="black" linkTo="../items/clothes"/>
-        <CategoryBox category="Accessories" backgroundColor="black" linkTo="../items/accessories"/>
+        <CategoryBox category="Shoes" backgroundImage={shoesImage} linkTo="../items/shoes" />
+        <CategoryBox category="Clothes" backgroundImage={clothesImage}  linkTo="../items/clothes" />
+        <CategoryBox category="Accessories" backgroundImage={accessoriesImage}  linkTo="../items/accessories" />
+      </div>
+      <h2>poupler items</h2>
+      <div className="item-list">
+        {!loading && pouplerItems.map((item, index) => (
+          <ItemBox key={item.id} item={item} />
+        ))}
+        {loading && (
+          <div className="loading-spinner">
+            <TailSpin height="80" width="80" color="blue" ariaLabel="loading" />
+          </div>
+        )}
       </div>
     </div>
   )
