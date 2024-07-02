bc55f06e35e1600a8f0d8f051fe7e5af6c7aea3c
Yoni4517 <yonistern15@gmail.com>
2024-07-02 22:52:02 +0300

    a

--- client/src/components/UpdateAd.jsx
+++ client/src/components/UpdateAd.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { TokenContext } from './TokenProvider';
-
+import '../style/updateAd.css'
 
 function UpdateAd({ ad, setShowUpdateAd }) {
 
